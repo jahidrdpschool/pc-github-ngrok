02ac52fb861ef1cf18b7e9af4e90213a5cf70da1
Jahid Hasan <130596733+jahidrdpschool@users.noreply.github.com>
2023-06-15 23:33:34 +0600

    Update STA.bat

--- STA.bat
+++ STA.bat
@@ -1,21 +1,24 @@
 @echo off
+
+set username=runneradmin
+set password=@#Jahid123456
+
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
 del /f "C:\Users\Public\Desktop\R 4.2.3.lnk" > errormsg.txt 2>&1
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > errormsg.txt 2>&1
 net config server /srvcomment:"Windows Server" > errormsg.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > errormsg.txt 2>&1
-net user Jahid @#Jahid123456 /add >nul
-net localgroup administrators Jahid /add >nul
+net user %username% %password% /add >nul
+net localgroup administrators %username% /add >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant Jahid:F >nul
-ICACLS C:\Windows\installer /grant Jahid:F >nul
+ICACLS C:\Windows\Temp /grant %username%:F >nul
+ICACLS C:\Windows\installer /grant %username%:F >nul
 echo ..........................................................
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: runneradmin
-echo Password: @#Jahid123456
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retrieve NGROK authtoken - check your authtoken again"
+echo Username: %username%
+echo Password: %password%
 echo Log in Now.
-
