97f9d0b8642e5c03248e57b4733dae58f5296175
Jahid Hasan <130596733+jahidrdpschool@users.noreply.github.com>
2023-06-15 22:44:30 +0600

    Update STA.bat

--- STA.bat
+++ STA.bat
@@ -15,7 +15,7 @@ ICACLS C:\Windows\installer /grant Jahid:F >nul
 echo ..........................................................
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: Jahid
+echo Username: runneradmin
 echo Password: @#Jahid123456
 echo Log in Now.
 
