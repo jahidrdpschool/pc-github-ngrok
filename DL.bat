c12319cec704d82af02cb9e9b1b6cf0ca7629fd2
Jahid Hasan <130596733+jahidrdpschool@users.noreply.github.com>
2023-06-15 12:26:38 +0600

    Up

--- DL.bat
+++ DL.bat
@@ -5,7 +5,7 @@ powershell -Command "choco install nssm -y"
 powershell -Command "choco install qbittorrent -y"
 powershell -Command "choco install anydesk -y"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/jahidrdpschool/pc-github-ngrok/main/ST.bat -OutFile ST.bat"
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/githubprojects41/Ngrok-Windows-RDP/main/loop.bat -OutFile loop.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/jahidrdpschool/pc-github-ngrok/main/LP.bat -OutFile LP.bat"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/jahidrdpschool/pc-github-ngrok/main/AC.bat -OutFile AC.bat"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/jahidrdpschool/pc-github-ngrok/main/UP.bat -OutFile UP.bat"
 
